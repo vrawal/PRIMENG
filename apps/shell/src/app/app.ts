79a2c2bfdf19888d15416cc87ebf05d6f329c3fd
vrawal <rawal_vishal@yahoo.com>
2025-10-24 23:51:07 +1000

    menu changes to have submenu items for admin and search menu item

--- apps/shell/src/app/app.ts
+++ apps/shell/src/app/app.ts
@@ -2,10 +2,22 @@ import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { MatButtonModule } from '@angular/material/button';
 import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatListModule } from '@angular/material/list';
+import { MatIconModule } from '@angular/material/icon';
+import { MatExpansionModule } from '@angular/material/expansion';
 
 @Component({
   standalone: true,
-  imports: [RouterModule, MatButtonModule, MatToolbarModule],
+  imports: [
+    RouterModule,
+    MatButtonModule,
+    MatToolbarModule,
+    MatSidenavModule,
+    MatListModule,
+    MatIconModule,
+    MatExpansionModule
+  ],
   selector: 'app-root',
   templateUrl: './app.html',
   styleUrl: './app.css',
