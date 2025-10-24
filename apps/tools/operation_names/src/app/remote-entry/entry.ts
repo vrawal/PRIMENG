790685a5a9bd964c9a75cf278dfa2e839f4c3510
vrawal <rawal_vishal@yahoo.com>
2025-10-24 23:43:09 +1000

    added material ui to operation names

--- apps/tools/operation_names/src/app/remote-entry/entry.ts
+++ apps/tools/operation_names/src/app/remote-entry/entry.ts
@@ -1,9 +1,22 @@
 import { Component } from '@angular/core';
 import { NxWelcome } from './nx-welcome';
+import { MatButtonModule } from '@angular/material/button';
+import { MatToolbarModule } from '@angular/material/toolbar';
 
 @Component({
-  imports: [NxWelcome],
-  selector: 'app-operation_names-entry',
-  template: `<app-nx-welcome></app-nx-welcome>`,
+  standalone: true,
+  imports: [NxWelcome, MatButtonModule, MatToolbarModule],
+  selector: 'app-operation-names-entry',
+  template: `
+    <mat-toolbar color="primary">
+      <span>Operation Names Module</span>
+    </mat-toolbar>
+    <app-nx-welcome></app-nx-welcome>
+  `,
+  styles: [`
+    :host {
+      display: block;
+    }
+  `]
 })
 export class RemoteEntry {}
