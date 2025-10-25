b84cd16265e8b681eed71f1dea42b1c6f955b830
vrawal <rawal_vishal@yahoo.com>
2025-10-26 09:56:34 +1000

    deploy to azure

--- apps/shell/webpack.prod.config.ts
+++ apps/shell/webpack.prod.config.ts
@@ -9,16 +9,11 @@ import config from './module-federation.config';
 export default withModuleFederation(
   {
     ...config,
-    /*
-     * Remote overrides for production.
-     * Each entry is a pair of a unique name and the URL where it is deployed.
-     *
-     * e.g.
-     * remotes: [
-     *   ['app1', 'https://app1.example.com'],
-     *   ['app2', 'https://app2.example.com'],
-     * ]
-     */
+    remotes: [
+      ['operation_names', 'https://primeng-app.azurestaticapps.net/operation_names/remoteEntry.js'],
+      ['vehicle', 'https://primeng-app.azurestaticapps.net/vehicle/remoteEntry.js'],
+      ['person', 'https://primeng-app.azurestaticapps.net/person/remoteEntry.js'],
+    ],
   },
   { dts: false }
 );
