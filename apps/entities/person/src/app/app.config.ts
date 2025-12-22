2d82f3897d962f12b8680fe469d565073d02eb21
vrawal <rawal_vishal@yahoo.com>
2025-12-22 10:05:07 +1000

    statemangement example

--- apps/entities/person/src/app/app.config.ts
+++ apps/entities/person/src/app/app.config.ts
@@ -2,11 +2,17 @@ import {
   ApplicationConfig,
   provideBrowserGlobalErrorListeners,
   provideZoneChangeDetection,
+  isDevMode,
 } from '@angular/core';
 import { provideRouter } from '@angular/router';
 import { provideAnimations } from '@angular/platform-browser/animations';
 import { provideHttpClient } from '@angular/common/http';
+import { provideStore } from '@ngrx/store';
+import { provideEffects } from '@ngrx/effects';
+import { provideStoreDevtools } from '@ngrx/store-devtools';
 import { appRoutes } from './app.routes';
+import { personReducer } from './store/person.reducer';
+import { PersonEffects } from './store/person.effects';
 
 export const appConfig: ApplicationConfig = {
   providers: [
@@ -15,5 +21,14 @@ export const appConfig: ApplicationConfig = {
     provideRouter(appRoutes),
     provideAnimations(),
     provideHttpClient(),
+    provideStore({ person: personReducer }),
+    provideEffects([PersonEffects]),
+    provideStoreDevtools({
+      maxAge: 25,
+      logOnly: !isDevMode(),
+      autoPause: true,
+      trace: false,
+      traceLimit: 75,
+    }),
   ],
 };
--- apps/entities/person/src/app/remote-entry/entry.routes.ts
+++ apps/entities/person/src/app/remote-entry/entry.routes.ts
@@ -1,4 +1,14 @@
 import { Route } from '@angular/router';
 import { RemoteEntry } from './entry';
+import { PersonListComponent } from '../components/person-list.component';
 
-export const remoteRoutes: Route[] = [{ path: '', component: RemoteEntry }];
+export const remoteRoutes: Route[] = [
+  {
+    path: '',
+    component: RemoteEntry,
+  },
+  {
+    path: 'list',
+    component: PersonListComponent,
+  },
+];
