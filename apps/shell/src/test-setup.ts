1e1d1ca843c3c912182eab0e5d181183a30dae2c
vrawal <rawal_vishal@yahoo.com>
2026-01-14 17:04:17 +1000

    vite?

--- apps/shell/src/test-setup.ts
+++ apps/shell/src/test-setup.ts
@@ -1,6 +1,13 @@
-import { setupZoneTestEnv } from 'jest-preset-angular/setup-env/zone';
+import 'zone.js';
+import 'zone.js/testing';
+import { getTestBed } from '@angular/core/testing';
+import {
+  BrowserDynamicTestingModule,
+  platformBrowserDynamicTesting,
+} from '@angular/platform-browser-dynamic/testing';
 
-setupZoneTestEnv({
-  errorOnUnknownElements: true,
-  errorOnUnknownProperties: true,
-});
+// Initialize the Angular testing environment
+getTestBed().initTestEnvironment(
+  BrowserDynamicTestingModule,
+  platformBrowserDynamicTesting()
+);
