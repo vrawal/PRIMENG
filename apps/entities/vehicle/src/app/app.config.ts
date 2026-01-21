f786593ca9556fb1d66e1584a3ae18c90f8d31ce
vrawal <rawal_vishal@yahoo.com>
2026-01-21 16:25:52 +1000

    fix: theming

--- apps/entities/vehicle/src/app/app.config.ts
+++ apps/entities/vehicle/src/app/app.config.ts
@@ -8,6 +8,28 @@ import { provideAnimations } from '@angular/platform-browser/animations';
 import { provideHttpClient } from '@angular/common/http';
 import { appRoutes } from './app.routes';
 
+import { providePrimeNG } from 'primeng/config';
+import Aura from '@primeuix/themes/aura';
+import { definePreset } from '@primeuix/themes';
+
+const MyPreset = definePreset(Aura, {
+    semantic: {
+        primary: {
+            50: '{orange.50}',
+            100: '{orange.100}',
+            200: '{orange.200}',
+            300: '{orange.300}',
+            400: '{orange.400}',
+            500: '{orange.500}',
+            600: '{orange.600}',
+            700: '{orange.700}',
+            800: '{orange.800}',
+            900: '{orange.900}',
+            950: '{orange.950}'
+        }
+    }
+});
+
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
@@ -15,5 +37,10 @@ export const appConfig: ApplicationConfig = {
     provideRouter(appRoutes),
     provideAnimations(),
     provideHttpClient(),
+    providePrimeNG({
+        theme: {
+            preset: MyPreset
+        }
+    })
   ],
 };
--- apps/shell/src/app/app.config.ts
+++ apps/shell/src/app/app.config.ts
@@ -10,6 +10,25 @@ import { appRoutes } from './app.routes';
 
 import { providePrimeNG } from 'primeng/config';
 import Aura from '@primeuix/themes/aura';
+import { definePreset } from '@primeuix/themes';
+
+const MyPreset = definePreset(Aura, {
+    semantic: {
+        primary: {
+            50: '{orange.50}',
+            100: '{orange.100}',
+            200: '{orange.200}',
+            300: '{orange.300}',
+            400: '{orange.400}',
+            500: '{orange.500}',
+            600: '{orange.600}',
+            700: '{orange.700}',
+            800: '{orange.800}',
+            900: '{orange.900}',
+            950: '{orange.950}'
+        }
+    }
+});
 
 export const appConfig: ApplicationConfig = {
   providers: [
@@ -20,7 +39,7 @@ export const appConfig: ApplicationConfig = {
     provideHttpClient(),
     providePrimeNG({
         theme: {
-            preset: Aura,
+            preset: MyPreset,
             options: {
                 darkModeSelector: '.my-app-dark'
             }
